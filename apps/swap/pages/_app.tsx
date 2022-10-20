8767e588066790cc053fb2f6bf290f54a9f96106
Hayden Fan <haosong0323@gmail.com>
2022-10-20 16:22:54 +0800

    feat(format, math): init format and math package

--- apps/swap/pages/_app.tsx
+++ apps/swap/pages/_app.tsx
@@ -5,14 +5,17 @@ import type { FC } from 'react'
 import { client } from '@zenlink-interface/wagmi'
 import { WagmiConfig } from 'wagmi'
 
+import { App } from '@zenlink-interface/ui'
 import { Header } from '../components'
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
    <>
     <WagmiConfig client={client}>
-      <Header />
-      <Component {...pageProps} />
+      <App.Shell>
+        <Header />
+        <Component {...pageProps} />
+      </App.Shell>
     </WagmiConfig>
    </>
   )
---
+++ packages/math/src/index.ts
@@ -0,0 +1,8 @@
+export { default as Big } from './Big'
+export type { default as BigintIsh } from './BigintIsh'
+export * from './constants'
+export { default as Decimal } from './Decimal'
+export { default as Fraction } from './Fraction'
+export { default as Percent } from './Percent'
+export { default as Rounding } from './Rounding'
+export { default as JSBI } from 'jsbi'
