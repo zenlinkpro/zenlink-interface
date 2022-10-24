20660d5cf2c2d0bd64e8e91b173e8a8204521f5d
Hayden Fan <haosong0323@gmail.com>
2022-10-24 18:23:25 +0800

    feat(redux-localstorage): add `redux-localstorage` package

---
+++ packages/redux/localstorage/slice.ts
@@ -0,0 +1,122 @@
+import type { PayloadAction, Slice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import type {
+  AddCustomToken,
+  AddCustomTokens,
+  ClearNotifications,
+  RemoveCustomToken,
+  StorageState,
+  UpdateCarbonOffsetPayload,
+  UpdateGasPrice,
+  UpdateGasType,
+  UpdateMaxFeePerGas,
+  UpdateMaxPriorityFeePerGas,
+  UpdateSlippageTolerancePayload,
+  UpdateSlippageToleranceTypePayload,
+  UpdateTransactionDeadline,
+  createNotification,
+} from './types'
+import { GasPrice } from './types'
+
+const parsedState = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('userPreferences') || '{}') : {}
+const initialState: StorageState = {
+  carbonOffset: parsedState?.carbonOffset || false,
+  slippageTolerance: parsedState?.slippageTolerance || 0.5,
+  slippageToleranceType: parsedState?.slippageToleranceType || 'auto',
+  gasPrice: parsedState?.gasPrice || GasPrice.HIGH,
+  maxFeePerGas: parsedState?.maxFeePerGas || undefined,
+  maxPriorityFeePerGas: parsedState?.maxPriorityFeePerGas || undefined,
+  gasType: parsedState?.gasType || 'preset',
+  customTokens: parsedState?.customTokens || {},
+  transactionDeadline: 30,
+  notifications: parsedState?.notifications || {},
+}
+
+const reducers = {
+  updateCarbonOffset: (state: StorageState, action: PayloadAction<UpdateCarbonOffsetPayload>) => {
+    const { carbonOffset } = action.payload
+    state.carbonOffset = carbonOffset
+  },
+  updateSlippageTolerance: (state: StorageState, action: PayloadAction<UpdateSlippageTolerancePayload>) => {
+    const { slippageTolerance } = action.payload
+    state.slippageTolerance = slippageTolerance
+  },
+  updateSlippageToleranceType: (state: StorageState, action: PayloadAction<UpdateSlippageToleranceTypePayload>) => {
+    const { slippageToleranceType } = action.payload
+    state.slippageToleranceType = slippageToleranceType
+  },
+  updateGasPrice: (state: StorageState, action: PayloadAction<UpdateGasPrice>) => {
+    const { gasPrice } = action.payload
+    state.gasPrice = gasPrice
+    state.gasType = 'preset'
+  },
+  updateMaxFeePerGas: (state: StorageState, action: PayloadAction<UpdateMaxFeePerGas>) => {
+    const { maxFeePerGas } = action.payload
+    state.maxFeePerGas = maxFeePerGas
+    if (state.maxPriorityFeePerGas)
+      state.gasType = 'custom'
+  },
+  updateMaxPriorityFeePerGas: (state: StorageState, action: PayloadAction<UpdateMaxPriorityFeePerGas>) => {
+    const { maxPriorityFeePerGas } = action.payload
+    state.maxPriorityFeePerGas = maxPriorityFeePerGas
+    if (state.maxFeePerGas)
+      state.gasType = 'custom'
+  },
+  updateGasType: (state: StorageState, action: PayloadAction<UpdateGasType>) => {
+    const { gasType } = action.payload
+    state.gasType = gasType
+  },
+  addCustomToken: (state: StorageState, action: PayloadAction<AddCustomToken>) => {
+    const { address, symbol, name, chainId, decimals } = action.payload
+
+    if (!state.customTokens[chainId])
+      state.customTokens[chainId] = {}
+
+    state.customTokens[chainId][address.toLowerCase()] = { address, symbol, name, chainId, decimals }
+  },
+  addCustomTokens: (state: StorageState, action: PayloadAction<AddCustomTokens>) => {
+    for (const item of action.payload) {
+      const { address, symbol, name, chainId, decimals } = item
+
+      if (!state.customTokens[chainId])
+        state.customTokens[chainId] = {}
+
+      state.customTokens[chainId][address.toLowerCase()] = { address, symbol, name, chainId, decimals }
+    }
+  },
+  removeCustomToken: (state: StorageState, action: PayloadAction<RemoveCustomToken>) => {
+    const { address, chainId } = action.payload
+
+    if (state.customTokens[chainId] && state.customTokens[chainId][address.toLowerCase()])
+      delete state.customTokens[chainId][address.toLowerCase()]
+  },
+  updateTransactionDeadline: (state: StorageState, action: PayloadAction<UpdateTransactionDeadline>) => {
+    const { transactionDeadline } = action.payload
+    state.transactionDeadline = transactionDeadline
+  },
+  createNotification: (state: StorageState, action: PayloadAction<createNotification>) => {
+    const { notification, account, timestamp } = action.payload
+    if (!state.notifications[account])
+      state.notifications[account] = {}
+
+    if (!state.notifications[account][timestamp])
+      state.notifications[account][timestamp] = [notification]
+
+    else
+      state.notifications[account][timestamp].push(notification)
+  },
+  clearNotifications: (state: StorageState, action: PayloadAction<ClearNotifications>) => {
+    const { account } = action.payload
+    delete state.notifications[account]
+  },
+}
+
+export function createStorageSlice(reducerPath: string): Slice<StorageState> {
+  return createSlice<StorageState, typeof reducers>({
+    name: reducerPath,
+    initialState,
+    reducers,
+  })
+}
+
+export type StorageActions = ReturnType<typeof createStorageSlice>['actions']
---
+++ packages/redux/localstorage/types.ts
@@ -0,0 +1,80 @@
+import type { ParachainId } from '@zenlink-interface/chain'
+
+export interface WithStorageState {
+  [path: string]: StorageState
+}
+
+export enum GasPrice {
+  INSTANT = 'Instant',
+  HIGH = 'High',
+  MEDIUM = 'Medium',
+  LOW = 'Low',
+}
+
+export interface TokenAsObject {
+  address: string
+  chainId: ParachainId
+  symbol?: string
+  name?: string
+  decimals: number
+}
+
+export interface StorageState {
+  carbonOffset: boolean
+  slippageTolerance: number
+  slippageToleranceType: 'auto' | 'custom'
+  gasPrice: GasPrice
+  maxFeePerGas: undefined | number
+  maxPriorityFeePerGas: undefined | number
+  gasType: 'custom' | 'preset'
+  customTokens: Record<number, Record<string, TokenAsObject>>
+  transactionDeadline: number
+  notifications: Record<string, Record<string, string[]>>
+}
+
+export interface UpdateCarbonOffsetPayload {
+  carbonOffset: boolean
+}
+
+export interface UpdateSlippageTolerancePayload {
+  slippageTolerance: number
+}
+
+export interface UpdateSlippageToleranceTypePayload {
+  slippageToleranceType: 'auto' | 'custom'
+}
+
+export interface UpdateGasPrice {
+  gasPrice: GasPrice
+}
+
+export interface UpdateMaxPriorityFeePerGas {
+  maxPriorityFeePerGas: number | undefined
+}
+
+export interface UpdateMaxFeePerGas {
+  maxFeePerGas: number | undefined
+}
+
+export interface UpdateGasType {
+  gasType: 'custom' | 'preset'
+}
+
+export interface UpdateTransactionDeadline {
+  transactionDeadline: number
+}
+
+export interface createNotification {
+  account: string
+  notification: string
+  timestamp: number
+}
+
+export interface ClearNotifications {
+  account: string
+}
+
+export type AddCustomToken = TokenAsObject
+export type AddCustomTokens = TokenAsObject[]
+export type RemoveCustomToken = Pick<TokenAsObject, 'chainId' | 'address'>
+
