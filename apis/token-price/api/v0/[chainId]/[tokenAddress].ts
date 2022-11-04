e356fb49189c565165c4559858aa2dfdbdf9d8b7
Hayden Fan <haosong0323@gmail.com>
2022-11-04 17:59:13 +0800

    feat(apis/token-price): apply [tokenAddress].ts

--- apis/token-price/api/v0/[chainId]/[tokenAddress].ts
+++ apis/token-price/api/v0/[chainId]/[tokenAddress].ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 import { SUPPORTED_CHAINS } from '../../../config'
+import redis from '../../../lib/redis'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const chainId = request.query.chainId as string
@@ -12,6 +13,20 @@ export default async (request: VercelRequest, response: VercelResponse) => {
       .json({ message: 'Unsupported network. Supported chain ids: '.concat(SUPPORTED_CHAINS.join(', ')) })
   }
 
+  const data = await redis.hget('prices', chainId)
+
+  if (!data)
+    return response.status(204)
+
+  const json = JSON.parse(data)
+  const result = json[tokenAddress]
+
+  if (!result) {
+    response
+      .status(400)
+      .json({ message: `Cannot find token with address ${tokenAddress}` })
+  }
+
   // price for [chainId] & [tokenAddress]
-  return response.status(200).send(`price for ${chainId} & ${tokenAddress}`)
+  return response.status(200).send(`price for ${chainId} & ${tokenAddress} is ${result}`)
 }
