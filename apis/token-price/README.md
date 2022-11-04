173f9ca944e8d85fcce49cbffdce3bb78f0d7fdf
Hayden Fan <haosong0323@gmail.com>
2022-11-04 15:17:57 +0800

    feat(api/token-price): api for token price

---
+++ apis/token-price/README.md
@@ -0,0 +1,5 @@
+# Price
+
+## v0
+
+### api/price/<CHAIN_ID>
---
+++ apis/token-price/config.ts
@@ -0,0 +1,6 @@
+import { ParachainId } from '@zenlink-interface/chain'
+
+export const SUPPORTED_CHAINS = [
+  ParachainId.ASTAR,
+  ParachainId.MOONRIVER,
+]
