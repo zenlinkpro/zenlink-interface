32112cde1a84576250fbdac979307bd3e2d85938
Hayden Fan <haosong0323@gmail.com>
2022-11-07 18:07:08 +0800

    feat(apps/pool): Layout components

--- apps/pool/pages/index.tsx
+++ apps/pool/pages/index.tsx
@@ -1,5 +1,18 @@
-function Pool() {
-  return <>123</>
+import { Layout } from 'components'
+
+function Pools() {
+  return (
+    <Layout>
+      <div className="flex flex-col gap-10 md:gap-16">
+        <section className="flex flex-col gap-6 lg:flex-row">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-2xl font-semibold text-slate-50">Pool</h2>
+            <p className="text-slate-300">Earn fees by providing liquidity.</p>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  )
 }
 
-export default Pool
+export default Pools
