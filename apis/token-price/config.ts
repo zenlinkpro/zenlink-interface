86579b1d1431b328a7d6ed1f4f6a001a88943ca4
Hayden Fan <haosong0323@gmail.com>
2022-11-07 14:55:11 +0800

    chore(apis/price): add chain moonbeam

--- apis/token-price/config.ts
+++ apis/token-price/config.ts
@@ -3,4 +3,5 @@ import { ParachainId } from '@zenlink-interface/chain'
 export const SUPPORTED_CHAINS = [
   ParachainId.ASTAR,
   ParachainId.MOONRIVER,
+  ParachainId.MOONBEAM,
 ]
--- scripts/price/src/config.ts
+++ scripts/price/src/config.ts
@@ -6,6 +6,7 @@ import fetch from 'cross-fetch'
 export const ZENLINK_CHAINS = [
   ParachainId.ASTAR,
   ParachainId.MOONRIVER,
+  ParachainId.MOONBEAM,
 ]
 
 const SQUID_HOST_ENDPOINT = 'https://squid.subsquid.io'
@@ -13,6 +14,7 @@ const SQUID_HOST_ENDPOINT = 'https://squid.subsquid.io'
 export const SQUID_HOST: Record<number | string, string> = {
   [ParachainId.ASTAR]: `${SQUID_HOST_ENDPOINT}/Zenlink-Astar-Squid/graphql`,
   [ParachainId.MOONRIVER]: `${SQUID_HOST_ENDPOINT}/zenlink-moonriver-squid/graphql`,
+  [ParachainId.MOONBEAM]: `${SQUID_HOST_ENDPOINT}/Zenlink-Moonbeam-Squid/graphql`,
 }
 
 export const CLIENTS: Record<number | string, ApolloClient<NormalizedCacheObject>> = {
@@ -36,6 +38,23 @@ export const CLIENTS: Record<number | string, ApolloClient<NormalizedCacheObject
   [ParachainId.MOONRIVER]: new ApolloClient({
     link: new HttpLink({
       uri: SQUID_HOST[ParachainId.MOONRIVER],
+      fetch,
+    }),
+    cache: new InMemoryCache(),
+    queryDeduplication: true,
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: 'no-cache',
+      },
+      query: {
+        fetchPolicy: 'no-cache',
+        errorPolicy: 'all',
+      },
+    },
+  }),
+  [ParachainId.MOONBEAM]: new ApolloClient({
+    link: new HttpLink({
+      uri: SQUID_HOST[ParachainId.MOONBEAM],
       fetch,
     }),
     cache: new InMemoryCache(),
