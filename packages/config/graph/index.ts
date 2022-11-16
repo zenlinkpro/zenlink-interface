344999ce132e1136cd475fce00a365d27f07a4bc
Hayden Fan <haosong0323@gmail.com>
2022-11-16 15:44:18 +0800

    chore(config/graph): update squid endpoint of moonriver

--- packages/config/graph/index.ts
+++ packages/config/graph/index.ts
@@ -10,6 +10,6 @@ export const SQUID_HOST_ENDPOINT = 'https://squid.subsquid.io'
 
 export const SQUID_HOST: Record<number | string, string> = {
   [ParachainId.ASTAR]: `${SQUID_HOST_ENDPOINT}/Zenlink-Astar-Squid/graphql`,
-  [ParachainId.MOONRIVER]: `${SQUID_HOST_ENDPOINT}/zenlink-moonriver-squid/graphql`,
+  [ParachainId.MOONRIVER]: `${SQUID_HOST_ENDPOINT}/Zenlink-Moonriver-Squid/graphql`,
   [ParachainId.MOONBEAM]: `${SQUID_HOST_ENDPOINT}/Zenlink-Moonbeam-Squid/graphql`,
 }
