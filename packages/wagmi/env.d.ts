2ef1e5f0fbbdc086e1e1cd8e335549df84e33af5
Hayden Fan <haosong0323@gmail.com>
2022-12-28 18:56:47 +0800

    fix(wagmi): declare SubWallet and talismanEth

--- packages/wagmi/env.d.ts
+++ packages/wagmi/env.d.ts
@@ -1,4 +1,8 @@
-declare interface Window {
-  talismanEth?: Window['ethereum']
-  SubWallet?: Window['ethereum']
+import type { Ethereum } from '@wagmi/core'
+
+declare global {
+  interface Window {
+    SubWallet?: Ethereum
+    talismanEth?: Ethereum
+  }
 }
