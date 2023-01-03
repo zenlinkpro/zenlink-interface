ada03b8e5e8d45e2829ed45486a90583808b2303
Hayden Fan <haosong0323@gmail.com>
2023-01-03 11:26:12 +0800

    fix(token-price): redis connection

--- apis/token-price/lib/redis.ts
+++ apis/token-price/lib/redis.ts
@@ -3,6 +3,10 @@ import Redis from 'ioredis'
 if (!process.env.REDIS_URL)
   throw new Error('REDIS_URL is required')
 
-const redis = new Redis(process.env.REDIS_URL)
+const redis = new Redis(process.env.REDIS_URL, {
+  tls: {
+    rejectUnauthorized: false,
+  },
+})
 
 export default redis
