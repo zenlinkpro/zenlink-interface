de63753e585a0cf7cd4d27970fc2178417f90d5a
Hayden Fan <haosong0323@gmail.com>
2023-02-14 00:13:35 +0800

    fix: next config

--- packages/config/nextjs/index.js
+++ packages/config/nextjs/index.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const defaultNextConfig = {
-  version: 2,
-  builds: [{ src: 'package.json', use: '@vercel/next' }],
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
   swcMinify: false,
   poweredByHeader: false,
   staticPageGenerationTimeout: 180,
-  experimental: {
-    esmExternals: 'loose',
-  },
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/dtdshj0e5/image/fetch',
