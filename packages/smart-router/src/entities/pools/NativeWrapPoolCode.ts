d4c0e52fef44c74649104067cee0b2c483140429
Hayden Fan <haosong0323@gmail.com>
2023-02-21 11:16:39 +0800

    fix(swap-router): code for `Unwrap Native`

--- packages/smart-router/src/entities/pools/NativeWrapPoolCode.ts
+++ packages/smart-router/src/entities/pools/NativeWrapPoolCode.ts
@@ -1,6 +1,6 @@
 import { CommandCode } from '../../CommandCode'
 import { HEXer } from '../../HEXer'
-import type { RouteLeg } from '../Graph'
+import type { MultiRoute, RouteLeg } from '../Graph'
 import type { NatvieWrapPool } from './NativeWrapPool'
 import { PoolCode } from './PoolCode'
 
@@ -13,7 +13,7 @@ export class NativeWrapPoolCode extends PoolCode {
     return PoolCode.RouteProcessorAddress
   }
 
-  public getSwapCodeForRouteProcessor(leg: RouteLeg): string {
+  public getSwapCodeForRouteProcessor(leg: RouteLeg, _route: MultiRoute, to: string): string {
     if (leg.tokenFrom.tokenId === this.pool.token0.tokenId) {
       // wrap - deposit. not used normally
       // wrapAndDistributeERC20Amounts
@@ -30,6 +30,7 @@ export class NativeWrapPoolCode extends PoolCode {
       const code = new HEXer()
         .uint8(CommandCode.UNWRAP_NATIVE)
         .address(this.pool.address)
+        .address(to)
         .toString()
       return code
     }
