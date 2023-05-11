8bab2fe9d8fe5acfe3b876ea8cdda6884c76140b
Hayden Fan <haosong0323@gmail.com>
2023-05-11 11:12:14 +0800

    fix: show displayValue in input

--- apps/swap/components/CurrencyInput.tsx
+++ apps/swap/components/CurrencyInput.tsx
@@ -6,6 +6,7 @@ import { ZERO } from '@zenlink-interface/math'
 import type { FC } from 'react'
 import { useMemo } from 'react'
 import type { Amount, Type } from '@zenlink-interface/currency'
+import { formatTransactionAmount } from '@zenlink-interface/format'
 import { useTrade } from './TradeProvider'
 
 interface _CurrencyInputProps extends CurrencyInputProps {
@@ -14,7 +15,7 @@ interface _CurrencyInputProps extends CurrencyInputProps {
   isWrap?: boolean
 }
 
-export const currencyAmountToPreciseFloat = (currencyAmount: Amount<Type> | undefined) => {
+const currencyAmountToPreciseFloat = (currencyAmount: Amount<Type> | undefined) => {
   if (!currencyAmount)
     return undefined
   const floatForLargerNumbers = parseFloat(currencyAmount.toExact())
@@ -45,12 +46,23 @@ export const CurrencyInput: FC<_CurrencyInputProps> = ({
   const { trade } = useTrade()
   const { data: prices } = usePrices({ chainId })
   // If output field and (un)wrapping, set to _value
-  let value = inputType === tradeType
-    ? _value
-    : trade
-      ? trade.outputAmount.toSignificant()
-      : ''
-  value = inputType === TradeType.EXACT_OUTPUT && isWrap ? _value : value
+  const [value, displayValue] = useMemo(() => {
+    let value = inputType === tradeType
+      ? _value
+      : trade
+        ? trade.outputAmount.toSignificant()
+        : ''
+    value = inputType === TradeType.EXACT_OUTPUT && isWrap ? _value : value
+
+    let displayValue = inputType === tradeType
+      ? _value
+      : trade
+        ? formatTransactionAmount(currencyAmountToPreciseFloat(trade.outputAmount))
+        : ''
+    displayValue = inputType === TradeType.EXACT_OUTPUT && isWrap ? _value : displayValue
+
+    return [value, displayValue]
+  }, [_value, inputType, isWrap, trade, tradeType])
 
   // Usd pct change
   const srcTokenPrice = trade?.inputAmount.currency ? prices?.[trade.inputAmount.currency.wrapped.address] : undefined
@@ -69,6 +81,7 @@ export const CurrencyInput: FC<_CurrencyInputProps> = ({
     <Web3Input.Currency
       className={className}
       value={value}
+      displayValue={displayValue}
       onChange={onChange}
       currency={currency}
       onSelect={onSelect}
