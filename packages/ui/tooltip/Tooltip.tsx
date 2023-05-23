e709b753bfd924038758d6e830c21b06f7149709
Hayden Fan <haosong0323@gmail.com>
2023-05-23 11:24:35 +0800

    feat(ui-tooltip): add `scale` transition when `floating` stuff mounted

--- packages/ui/tooltip/Tooltip.tsx
+++ packages/ui/tooltip/Tooltip.tsx
@@ -31,7 +31,7 @@ export const Tooltip: FC<TooltipProps> = ({ children, content, placement = 'top'
   const [open, setOpen] = useState(false)
   const arrowRef = useRef(null)
 
-  const { refs, floatingStyles, context } = useFloating({
+  const { x, y, refs, floatingStyles, context } = useFloating({
     placement,
     open,
     onOpenChange: setOpen,
@@ -60,7 +60,12 @@ export const Tooltip: FC<TooltipProps> = ({ children, content, placement = 'top'
     role,
   ])
 
-  const { isMounted, styles } = useTransitionStyles(context, { duration: 300 })
+  const { isMounted, styles } = useTransitionStyles(context, {
+    initial: {
+      opacity: 0,
+      transform: 'scale(0.8)',
+    },
+  })
 
   return (
     <>
@@ -74,19 +79,21 @@ export const Tooltip: FC<TooltipProps> = ({ children, content, placement = 'top'
             style={{
               ...floatingStyles,
               ...styles,
+              left: x ?? 0,
+              top: y ?? 0,
             }}
             {...getFloatingProps()}
             className={classNames('z-[9998] rounded-xl border border-slate-400 dark:border-slate-700 shadow-[0px_4px_8px_0px_rgba(0,0,0,0.20)]', className)}
           >
+            <div className="bg-slate-50 dark:bg-slate-800 rounded-xl text-slate-700 dark:text-slate-300 font-medium px-2.5 py-2 text-xs leading-normal">
+              {content}
+            </div>
             <FloatingArrow
               strokeWidth={1}
               className="fill-slate-50 dark:fill-slate-800 [&>path:first-of-type]:stroke-slate-400 dark:[&>path:first-of-type]:stroke-slate-700 [&>path:last-of-type]:stroke-slate-50 dark:[&>path:last-of-type]:stroke-slate-800"
               ref={arrowRef}
               context={context}
             />
-            <div className="bg-slate-50 dark:bg-slate-800 rounded-xl text-slate-700 dark:text-slate-300 font-medium px-2.5 py-2 text-xs leading-normal">
-              {content}
-            </div>
           </div>
         </FloatingPortal>
       )}
