8b9f5d39014ac9b7dcf3b5140445819067feec95
Hayden Fan <haosong0323@gmail.com>
2023-06-04 22:05:39 +0800

    chore: update deps

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -35,6 +35,7 @@ const eslintConfig = {
     'no-mixed-operators': 'off',
     'max-statements-per-line': ['error', { max: 2 }],
     'antfu/top-level-function': 'off',
+    'no-restricted-globals': 'off',
   },
 }
 
