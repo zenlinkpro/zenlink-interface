90d729636702b69646d74fe5db57f8f9d4955a3d
haike0513 <81543786+haike0513@users.noreply.github.com>
2023-06-05 16:18:50 +0800

    fix(graph-client): use encodedChainAddress to fetch userPools  (#115)
    * fix(pool): fixed the my positions list on substrate chain
    
    * wip: fix
    
    * chore: clean
    
    * wip: fix
    
    ---------
    
    Co-authored-by: Hayden Fan <haosong0323@gmail.com>

--- packages/compat/config/chain.ts
+++ packages/compat/config/chain.ts
@@ -1,28 +1,18 @@
 import { ParachainId } from '@zenlink-interface/chain'
 
+export {
+  EVM_NETWORKS,
+  isEvmNetwork,
+  isSubstrateNetwork,
+  SUBSTRATE_NETWORKS,
+} from '@zenlink-interface/chain'
+
 export const AMM_ENABLED_NETWORKS = [
   ParachainId.ASTAR,
   ParachainId.MOONRIVER,
   ParachainId.MOONBEAM,
-]
-
-export const EVM_NETWORKS = [
-  ParachainId.ASTAR,
-  ParachainId.MOONRIVER,
-  ParachainId.MOONBEAM,
-]
-
-export const SUBSTRATE_NETWORKS = [
   ParachainId.BIFROST_KUSAMA,
   ParachainId.BIFROST_POLKADOT,
 ]
 
 export const SUPPORTED_CHAIN_IDS = Array.from(new Set([...AMM_ENABLED_NETWORKS]))
-
-export function isEvmNetwork(chainId: ParachainId) {
-  return EVM_NETWORKS.includes(chainId)
-}
-
-export function isSubstrateNetwork(chainId: ParachainId) {
-  return SUBSTRATE_NETWORKS.includes(chainId)
-}
