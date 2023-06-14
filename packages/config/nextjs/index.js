c09cbd971bddc9d825b741970698891bffb1c64e
Vern Zhang <vern@manta.network>
2023-06-14 11:24:11 +0800

    chore: update strict model as false

--- packages/config/nextjs/index.js
+++ packages/config/nextjs/index.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const defaultNextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   productionBrowserSourceMaps: true,
   swcMinify: false,
   poweredByHeader: false,
