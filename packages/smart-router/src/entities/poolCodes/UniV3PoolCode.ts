7d2da67949fe61d3ee392dd6214596d0c9cf28eb
Hayden Fan <haosong0323@gmail.com>
2023-07-25 18:26:23 +0800

    chore: add uniswap executor

--- packages/smart-router/src/entities/poolCodes/UniV3PoolCode.ts
+++ packages/smart-router/src/entities/poolCodes/UniV3PoolCode.ts
@@ -1,15 +1,33 @@
 import type { RouteLeg, SplitMultiRoute } from '@zenlink-interface/amm'
+import { ParachainId } from '@zenlink-interface/chain'
+import invariant from 'tiny-invariant'
+import type { Address } from 'viem'
+import { encodeAbiParameters, parseAbiParameters } from 'viem'
 import { HEXer } from '../../HEXer'
 import type { UniV3Pool } from '../pools/UniV3Pool'
 import { PoolCode } from './PoolCode'
 
 export class UniV3PoolCode extends PoolCode {
+  executor: { [chainId: number]: string } = {
+    [ParachainId.SCROLL_TESTNET]: '0x1B5a2f88420ff329406D108e641e52E46465F68e',
+  } as const
+
   public constructor(pool: UniV3Pool, providerName: string) {
     super(pool, providerName)
   }
 
   public override getStartPoint(): string {
     return PoolCode.RouteProcessorAddress
+  }
+
+  public getProtocolExecutor(): string {
+    const chainId = this.pool.token0.chainId
+    invariant(chainId !== undefined, 'UniV3PoolCode: Unseted chainId')
+    return this.executor[Number(chainId)]
+  }
+
+  public override getProtocolExecutorStartPoint(): string {
+    return this.getProtocolExecutor()
   }
 
   public getSwapCodeForRouteProcessor(_leg: RouteLeg, _route: SplitMultiRoute, _to: string): string {
@@ -22,6 +40,23 @@ export class UniV3PoolCode extends PoolCode {
       .address(this.pool.address)
       .bool(leg.tokenFrom.address === this.pool.token0.address)
       .address(to)
+      .toString()
+    return code
+  }
+
+  public override getSwapCodeForAggregationRouter(leg: RouteLeg, _route: SplitMultiRoute, to: string): string {
+    const code = new HEXer()
+      .address(this.getProtocolExecutor())
+      .bytes(
+        encodeAbiParameters(
+          parseAbiParameters('address, bool, address'),
+          [
+            this.pool.address as Address,
+            leg.tokenFrom.address === this.pool.token0.address,
+            to as Address,
+          ],
+        ),
+      )
       .toString()
     return code
   }
