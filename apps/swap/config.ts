439a4b7f396206fd9da25096ec381d9555543cd3
Hayden Fan <haosong0323@gmail.com>
2023-07-27 11:33:47 +0800

    chore: remove moonbeam from config

--- apps/swap/config.ts
+++ apps/swap/config.ts
@@ -11,13 +11,13 @@ export const AMM_ENABLED_NETWORKS = [
 export const AGGREGATOR_ENABLED_NETWORKS = [
   ParachainId.ASTAR,
   ParachainId.ARBITRUM_ONE,
-  ParachainId.MOONBEAM,
+  // ParachainId.MOONBEAM,
 ]
 
 export const REFERRALS_ENABLED_NETWORKS = [
   ParachainId.ASTAR,
   ParachainId.ARBITRUM_ONE,
-  ParachainId.MOONBEAM,
+  // ParachainId.MOONBEAM,
 ]
 
 export const SUPPORTED_CHAIN_IDS = Array.from(
