90233261f69dec66f118f3b8884a17d255a2afe9
Marcel Ebert <mail@marcel-ebert.de>
2023-08-09 18:06:53 +0200

    Temporarily change graph config for amplitude to foucoco

--- packages/config/graph/src/index.ts
+++ packages/config/graph/src/index.ts
@@ -15,14 +15,14 @@ export const SQUID_HOST: Record<number | string, string> = {
   [ParachainId.ASTAR]: `${SQUID_HOST_ENDPOINT}/Zenlink-Astar-Squid/graphql`,
   [ParachainId.MOONRIVER]: `${SQUID_HOST_ENDPOINT}/Zenlink-Moonriver-Squid/graphql`,
   [ParachainId.MOONBEAM]: `${SQUID_HOST_ENDPOINT}/Zenlink-Moonbeam-Squid/graphql`,
-  [ParachainId.AMPLITUDE]: `${SQUID_HOST_ENDPOINT}/amplitude-squid/graphql`,
+  [ParachainId.AMPLITUDE]: `${SQUID_HOST_ENDPOINT}/foucoco-squid/graphql`,
   [ParachainId.BIFROST_KUSAMA]: `${SQUID_HOST_ENDPOINT}/zenlink-bifrost-kusama-squid/graphql`,
   [ParachainId.BIFROST_POLKADOT]: `${SQUID_HOST_ENDPOINT}/zenlink-bifrost-polkadot-squid/graphql`,
   [ParachainId.ARBITRUM_ONE]: 'https://api.thegraph.com/subgraphs/name/ianlapham/arbitrum-minimal',
 }
 
 export const ARCHIVE_HOST: Record<number | string, string> = {
-  [ParachainId.AMPLITUDE]: 'https://amplitude.explorer.subsquid.io/graphql',
+  [ParachainId.AMPLITUDE]: 'https://foucoco.explorer.subsquid.io/graphql',
   [ParachainId.BIFROST_KUSAMA]: 'https://bifrost.explorer.subsquid.io/graphql',
   [ParachainId.BIFROST_POLKADOT]: 'https://bifrost-polkadot.explorer.subsquid.io/graphql',
 }
