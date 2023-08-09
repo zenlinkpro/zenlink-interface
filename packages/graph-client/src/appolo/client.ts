b616fdb31d54e257cfcdb64babbfeb1601792cf5
Marcel Ebert <mail@marcel-ebert.de>
2023-08-09 18:06:26 +0200

    Add Amplitude to apollo clients

--- packages/graph-client/src/appolo/client.ts
+++ packages/graph-client/src/appolo/client.ts
@@ -47,6 +47,11 @@ export const CLIENTS: Record<number | string, ApolloClient<NormalizedCacheObject
     link: createLink(ParachainId.ARBITRUM_ONE),
     cache: new InMemoryCache(),
   }),
+  [ParachainId.AMPLITUDE]: new ApolloClient({
+    ...DEFAULT_CLIENT_OPTIONS,
+    link: createLink(ParachainId.AMPLITUDE),
+    cache: new InMemoryCache(),
+  }),
 }
 
 export const ARCHIVE_CLIENTS: Record<number | string, ApolloClient<NormalizedCacheObject>> = {
@@ -58,6 +63,11 @@ export const ARCHIVE_CLIENTS: Record<number | string, ApolloClient<NormalizedCac
   [ParachainId.BIFROST_POLKADOT]: new ApolloClient({
     ...DEFAULT_CLIENT_OPTIONS,
     link: createLink(ParachainId.BIFROST_POLKADOT, { useArchive: true }),
+    cache: new InMemoryCache(),
+  }),
+  [ParachainId.AMPLITUDE]: new ApolloClient({
+    ...DEFAULT_CLIENT_OPTIONS,
+    link: createLink(ParachainId.AMPLITUDE, { useArchive: true }),
     cache: new InMemoryCache(),
   }),
 }
