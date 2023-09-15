6a2df530e33cd37147b61bc5080fec8276fda2da
Hayden Fan <haosong0323@gmail.com>
2023-09-15 12:01:52 +0800

    refactor: gmx pool

--- packages/smart-router/src/entities/pools/GmxPool.ts
+++ packages/smart-router/src/entities/pools/GmxPool.ts
@@ -1,13 +1,15 @@
-import type { BigNumber } from '@ethersproject/bignumber'
+import { BigNumber } from '@ethersproject/bignumber'
 import type { BaseToken } from '@zenlink-interface/amm'
 import { Token } from '@zenlink-interface/currency'
 import { zeroAddress } from 'viem'
-import { getNumber } from '../../util'
+import { getBigNumber, getNumber } from '../../util'
 import { BasePool, TYPICAL_MINIMAL_LIQUIDITY, TYPICAL_SWAP_GAS_COST } from './BasePool'
 
 function adjustForDecimals(amount: number, tokenDiv: Token, tokenMul: Token): number {
   return amount * (10 ** tokenMul.decimals) / (10 ** tokenDiv.decimals)
 }
+
+const MinimalDiffUsdgAmount = BigNumber.from(1000).mul(BigNumber.from(10).pow(18))
 
 export class GmxPool extends BasePool {
   private readonly _USDG: Token
@@ -96,6 +98,32 @@ export class GmxPool extends BasePool {
     this.token1MinPrice = token1MinPrice
   }
 
+  private _checkDecreasePoolAmount(isToken0: boolean, amount: BigNumber): boolean {
+    const reserve = isToken0 ? this.reserve0 : this.reserve1
+    const price = isToken0 ? this.token0MinPrice : this.token1MinPrice
+    let usdgAmount = getNumber(reserve) * getNumber(price) / 10 ** 30
+    usdgAmount = adjustForDecimals(usdgAmount, isToken0 ? this._token0 : this._token1, this._USDG)
+
+    if (usdgAmount < getNumber(MinimalDiffUsdgAmount))
+      return false
+    if (reserve.lt(amount))
+      return false
+    return true
+  }
+
+  private _checkIncreaseUsdgAmount(isToken0: boolean, amount: BigNumber): boolean {
+    const usdgAmount = isToken0 ? this.usdgAmount0 : this.usdgAmount1
+    const maxUsdgAmount = isToken0 ? this.maxUsdgAmount0 : this.maxUsdgAmount1
+    if (!maxUsdgAmount.eq(0)) {
+      if (maxUsdgAmount.sub(usdgAmount).lt(MinimalDiffUsdgAmount))
+        return false
+      if (usdgAmount.add(amount).gt(maxUsdgAmount))
+        return false
+    }
+
+    return true
+  }
+
   public getOutput(amountIn: number, direction: boolean): { output: number; gasSpent: number } {
     const priceIn = direction ? this.token0MinPrice : this.token1MinPrice
     const priceOut = direction ? this.token1MaxPrice : this.token0MaxPrice
@@ -109,6 +137,12 @@ export class GmxPool extends BasePool {
     let usdgAmount = amountIn * getNumber(priceIn) / 10 ** 30
     usdgAmount = adjustForDecimals(usdgAmount, direction ? this._token0 : this._token1, this._USDG)
 
+    if (
+      !this._checkIncreaseUsdgAmount(direction, getBigNumber(usdgAmount))
+      || !this._checkDecreasePoolAmount(!direction, getBigNumber(amountOutAfterAdjustDecimals))
+    )
+      return { output: 0, gasSpent: this.swapGasCost }
+
     const taxFee = amountOutAfterAdjustDecimals * (this.isStable ? this.stableTaxFee : this.taxFee) / reserveOut
 
     return { output: amountOutAfterAdjustDecimals * (1 - this.fee - taxFee), gasSpent: this.swapGasCost }
@@ -119,6 +153,8 @@ export class GmxPool extends BasePool {
     const priceOut = direction ? this.token1MaxPrice : this.token0MaxPrice
 
     const reserveOut = direction ? getNumber(this.reserve1) : getNumber(this.reserve0)
+    if (!this._checkDecreasePoolAmount(!direction, getBigNumber(amountOut)))
+      return { input: Number.POSITIVE_INFINITY, gasSpent: this.swapGasCost }
 
     const taxFee = amountOut * (this.isStable ? this.stableTaxFee : this.taxFee) / reserveOut
     const amountOutBeforeFee = amountOut / (1 - this.fee - taxFee)
@@ -130,16 +166,21 @@ export class GmxPool extends BasePool {
     )
     let usdgAmount = amountInAfterAdjustDecimals * getNumber(priceIn) / 10 ** 30
     usdgAmount = adjustForDecimals(usdgAmount, direction ? this._token0 : this._token1, this._USDG)
+    if (!this._checkIncreaseUsdgAmount(direction, getBigNumber(usdgAmount)))
+      return { input: Number.POSITIVE_INFINITY, gasSpent: this.swapGasCost }
+
     return { input: amountInAfterAdjustDecimals, gasSpent: this.swapGasCost }
   }
 
   public calcCurrentPriceWithoutFee(direction: boolean): number {
     const priceIn = direction ? this.token0MinPrice : this.token1MinPrice
     const priceOut = direction ? this.token1MaxPrice : this.token0MaxPrice
-    return adjustForDecimals(
+    const price = adjustForDecimals(
       getNumber(priceIn) / getNumber(priceOut),
       direction ? this._token0 : this._token1,
       direction ? this._token1 : this._token0,
     )
+
+    return price
   }
 }
