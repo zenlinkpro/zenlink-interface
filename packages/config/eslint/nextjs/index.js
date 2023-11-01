9ea34fa7205840b1c80789b41a49601c97a32b54
Hayden Fan <haosong0323@gmail.com>
2023-11-01 16:19:46 +0800

    clean

--- packages/config/eslint/nextjs/index.js
+++ packages/config/eslint/nextjs/index.js
@@ -44,32 +44,3 @@ module.exports = antfu(
     },
   }
 )
-
-// // @ts-check
-// /** @type {import('eslint').ESLint.ConfigData} */
-// const eslintConfig = {
-//   root: true,
-//   extends: ['@antfu/eslint-config'],
-//   plugins: ['testing-library', 'simple-import-sort', 'unused-imports'],
-//   rules: {
-//     'simple-import-sort/exports': 'warn',
-//     'react/display-name': 'off',
-//     'unused-imports/no-unused-imports': 'warn',
-//     'unused-imports/no-unused-vars': 'warn',
-//     'no-unused-vars': 'warn',
-//     'no-template-curly-in-string': 'off',
-//     'no-use-before-define': 'off',
-//     '@typescript-eslint/no-empty-function': 'off',
-//     '@typescript-eslint/no-use-before-define': 'off',
-//     'react-hooks/exhaustive-deps': 'error',
-//     'react/prop-types': 'off',
-//     'no-mixed-operators': 'off',
-//     'max-statements-per-line': ['error', { max: 2 }],
-//     'antfu/top-level-function': 'off',
-//     'no-restricted-globals': 'off',
-//     '@typescript-eslint/no-unsafe-declaration-merging': 'off',
-//     'n/prefer-global/process': 'off',
-//   },
-// }
-
-// module.exports = eslintConfig
