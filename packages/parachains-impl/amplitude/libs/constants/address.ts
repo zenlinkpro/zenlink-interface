102329c52b1671b033a8bc336c3495b38851a952
Gianfranco Tasteri <gianfra33@gmail.com>
2024-01-30 13:37:09 -0300

    add some lp token data

--- packages/parachains-impl/amplitude/libs/constants/address.ts
+++ packages/parachains-impl/amplitude/libs/constants/address.ts
@@ -8,28 +8,25 @@ export const PAIR_ADDRESSES: Record<string, { address: string, account: string }
     account: '6jM63XCYUjHhdfXy9YMa2fKa9z4nqyJTb4DQxjm5mqsXBBQ5',
   },
   // PEN-DOT
+  // TODO replace these values with the actual values when the tx passes
   '2094-0-0-2094-2-256': {
-    //TODO where to get this for Pendulum? Especially the account.
     address: '2094-2-1099511628544',
     account: '6jM63XCYUjHhdfXy9YMa2fKa9z4nqyJTb4DQxjm5mqsXBBQ5',
   },
   // PEN-GLMR
   '2094-0-0-2094-2-262': {
-    //TODO where to get this for Pendulum? Especially the account.
-    address: '2094-2-1099511628544',
-    account: '6jM63XCYUjHhdfXy9YMa2fKa9z4nqyJTb4DQxjm5mqsXBBQ5',
+    address: '2094-2-1125281432320',
+    account: '6dZRnXf96SQyUc8WF1Uhr75nZTGs3GaUZzpQX3nPLE52XTnj',
   },
   // XLM-DOT
   '2094-2-256-2094-2-512': {
-    //TODO where to get this for Pendulum? Especially the account.
-    address: '2094-2-1099511628544',
-    account: '6jM63XCYUjHhdfXy9YMa2fKa9z4nqyJTb4DQxjm5mqsXBBQ5',
+    address: '2094-2-2199040033536',
+    account: '6dZRnXf96SQyUc8WF1UfP9oTVXysJVgXT1wQWFg1hXqYiPbC',
   },
    // PEN-XLM
    '2094-0-0-2094-2-512': {
-    //TODO where to get this for Pendulum? Especially the account.
-    address: '2094-2-1099511628544',
-    account: '6jM63XCYUjHhdfXy9YMa2fKa9z4nqyJTb4DQxjm5mqsXBBQ5',
+    address: '2094-2-2199023256320',
+    account: '6dZRnXf96SQyUc8WF1UjuSqPCVsQkNKz1sunYim24vc2L6sp',
   },
 }
 
