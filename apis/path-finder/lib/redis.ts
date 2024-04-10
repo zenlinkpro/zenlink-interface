f538f63f56681eae8fbf255e2f156085c52ddda2
Hayden Fan <haosong0323@gmail.com>
2024-04-10 17:27:06 +0800

    feat(path-finder): request limit for v2 api

---
+++ apis/path-finder/lib/redis.ts
@@ -0,0 +1,12 @@
+import Redis from 'ioredis'
+
+if (!process.env.REDIS_URL)
+  throw new Error('REDIS_URL is required')
+
+const redis = new Redis(process.env.REDIS_URL, {
+  tls: {
+    rejectUnauthorized: false,
+  },
+})
+
+export default redis
