1cda17ab728e30f684a979c117c603faee6eef16
Hayden Fan <haosong0323@gmail.com>
2024-06-26 22:33:58 +0800

    wip

--- packages/market/src/__tests__/MarketParams.test.ts
+++ packages/market/src/__tests__/MarketParams.test.ts
@@ -19,4 +19,40 @@ describe('MarketParams', () => {
       expect(calcFee(0.5)).toMatchInlineSnapshot(`4987541511000000`)
     })
   })
+
+  describe('vDOT JUL-2025', () => {
+    it('Parameters', () => {
+      const rateMin = 0.12
+      const rateMax = 0.24
+      const startTimestamp = 1719407555
+      const endTimestamp = 1751500800
+      expect(calculateParameters(rateMin, rateMax, startTimestamp, endTimestamp)).toMatchInlineSnapshot(`
+        {
+          "initialRateAnchor": 1183483673000000000,
+          "scalarRoot": 36513737144000000000,
+        }
+      `)
+    })
+    it('LnFeeRateRoot', () => {
+      expect(calcFee(0.3)).toMatchInlineSnapshot('2995508979000000')
+    })
+  })
+
+  describe('vGLMR JUL-2025', () => {
+    it('Parameters', () => {
+      const rateMin = 0.04
+      const rateMax = 0.12
+      const startTimestamp = 1719407555
+      const endTimestamp = 1751500800
+      expect(calculateParameters(rateMin, rateMax, startTimestamp, endTimestamp)).toMatchInlineSnapshot(`
+        {
+          "initialRateAnchor": 1081483044000000000,
+          "scalarRoot": 54856182234000000000,
+        }
+      `)
+    })
+    it('LnFeeRateRoot', () => {
+      expect(calcFee(0.3)).toMatchInlineSnapshot('2995508979000000')
+    })
+  })
 })
