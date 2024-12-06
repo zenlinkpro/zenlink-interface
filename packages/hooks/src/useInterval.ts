9278a4e108128aec3538e9cdb7ac291c2b006578
Hayden Fan <haosong0323@gmail.com>
2024-12-06 19:35:20 +0800

    fix

--- packages/hooks/src/useInterval.ts
+++ packages/hooks/src/useInterval.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 
 export function useInterval(callback: () => void, delay: null | number, leading = true) {
-  const savedCallback = useRef<() => void>()
+  const savedCallback = useRef<() => void>(null)
 
   // Remember the latest callback.
   useEffect(() => {
